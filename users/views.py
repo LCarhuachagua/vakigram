b25e11292c5f470d4b97a17a9c2dfbc364653d6a
Laly Carhuachagua <roselyn199206@gmail.com>
2023-09-12 16:23:50 -0500

    register create with validations fr passw equals

--- users/views.py
+++ users/views.py
@@ -4,6 +4,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+# exceptions
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
 # Create your views here.
 def login_view(request):
     """login view."""
@@ -24,3 +31,24 @@ def logout_view(request):
     """logout view."""
     logout(request)
     return redirect('login')
+
+def signup(request):
+    """signup view."""
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+        
+        user = User.objects.create_user(username=username, password=passwd)
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+    return render(request, 'users/signup.html')
