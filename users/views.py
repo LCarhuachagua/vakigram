98405d49b56756b68ec55370edd182ca42683aeb
Laly Carhuachagua <roselyn199206@gmail.com>
2023-09-15 11:17:43 -0500

    update signup error user already

--- users/views.py
+++ users/views.py
@@ -41,11 +41,15 @@ def signup(request):
 
         if passwd != passwd_confirmation:
             return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in use'})
         
-        user = User.objects.create_user(username=username, password=passwd)
         user.first_name = request.POST['first_name']
         user.last_name = request.POST['last_name']
         user.email = request.POST['email']
+        user.save()
 
         profile = Profile(user=user)
         profile.save()
