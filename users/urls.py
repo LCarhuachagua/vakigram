75b47b67340b86204ba0fb6f233a1a76605eda14
Laly Carhuachagua <roselyn199206@gmail.com>
2023-09-18 16:19:40 -0500

    Detail View - protect the view

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name="logout"),
     path('signup/', views.signup, name="signup"), 
-    path('me/profile/', views.update_profile, name="update_profile"),
+    path('me/profile/', views.update_profile, name="update"),
+    path('<str:username>/', views.UserDetailView.as_view(), name='detail'), 
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,10 @@
 # Django
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import DetailView
 
 # exceptions
 from django.db.utils import IntegrityError
@@ -10,9 +13,10 @@ from django.db.utils import IntegrityError
 # Models
 from django.contrib.auth.models import User
 from users.models import Profile
+from posts.models import Post
 
 # forms
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 # Create your views here.
 
@@ -69,27 +73,60 @@ def logout_view(request):
     logout(request)
     return redirect('login')
 
+class UserDetailView(LoginRequiredMixin, DetailView):
+    """User detail view."""
+
+    template_name = 'users/detail.html'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    queryset = User.objects.all()
+    context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        """Add user's posts to context."""
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        context['posts'] = Post.objects.filter(user=user).order_by('-created')
+        return context
+
+
 def signup(request):
     """signup view."""
     if request.method == 'POST':
-        username = request.POST['username']
-        passwd = request.POST['passwd']
-        passwd_confirmation = request.POST['passwd_confirmation']
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(
+        request = request,
+        template_name = 'users/signup.html',
+        context = {
+            'form': form
+        }
+    )
 
-        if passwd != passwd_confirmation:
-            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
-        try:
-            user = User.objects.create_user(username=username, password=passwd)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already in use'})
-        
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
 
-        profile = Profile(user=user)
-        profile.save()
+    #if request.method == 'POST':
+    #    username = request.POST['username']
+    #    passwd = request.POST['passwd']
+    #    passwd_confirmation = request.POST['passwd_confirmation']
 
-        return redirect('login')
-    return render(request, 'users/signup.html')
+    #    if passwd != passwd_confirmation:
+    #        return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+    #    try:
+    #        user = User.objects.create_user(username=username, password=passwd)
+    #    except IntegrityError:
+    #        return render(request, 'users/signup.html', {'error': 'Username is already in use'})
+    #    
+    #    user.first_name = request.POST['first_name']
+    #    user.last_name = request.POST['last_name']
+    #    user.email = request.POST['email']
+    #    user.save()
+
+    #profile = Profile(user=user)
+    #    profile.save()
+
+    #    return redirect('login')
+    #   return render(request, 'users/signup.html')
