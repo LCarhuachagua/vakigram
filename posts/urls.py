fbfaa55a0852b4d8dd66a4ffd39cd573232e038a
Laly Carhuachagua <roselyn199206@gmail.com>
2023-09-19 11:03:48 -0500

    updating classbased and htmls

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.PostsFeedView.as_view(), name='feed'),
     path('posts/new/', views.create_post, name='created_post'),
+    path(
+        route='posts/<int:pk>/',
+        view= views.PostDetailView.as_view(),
+        name='detail_post'
+    )
 ]
--- posts/views.py
+++ posts/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 #from django.shortcuts import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Forms
 from posts.forms import PostForm
@@ -23,9 +23,15 @@ class PostsFeedView(LoginRequiredMixin, ListView):
    template_name = 'posts/feed.html'
    model = Post
    ordering = ('-created',)
-   paginate_by = 2
+   paginate_by = 30
    context_object_name = 'posts'
 
+class PostDetailView(LoginRequiredMixin, DetailView):
+   """Return post detail."""
+
+   template_name = 'posts/detail.html'
+   queryset = Post.objects.all()
+   context_object_name = 'post'
 
 #@login_required
 #def list_posts(request):
